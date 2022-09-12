baea2b12dcd6e3873407ee20dac5552fb8803e85
Joakim Johansson <johansson.joakim@gmail.com>
2022-09-12 08:12:32 +0200

    Timer

--- breathing WatchKit Extension/Views/CircleOfLife.swift
+++ breathing WatchKit Extension/Views/CircleOfLife.swift
@@ -12,6 +12,7 @@ struct CircleOfLife: View {
     @State private var startAnimation: Double = 0
     @State private var endAnimation: Double = 0
     @State private var themeColor: Color = Color.white.opacity(0.3)
+    @State private var timer: Double = 0
     let level: Level
     let radius: CGFloat = 100
     let pi = Double.pi
@@ -19,10 +20,10 @@ struct CircleOfLife: View {
     let dotLength: CGFloat = 5
     let spaceLength: CGFloat
     let delayAnimation: Double = 1.5
-
     
     init(level: Level) {
         self.level = level
+        self.timer2 = Timer.publish(every: level.inhale.msToSeconds, on: .main, in: .common).autoconnect()
         let circumerence: CGFloat = CGFloat(2.0 * pi) * radius
         spaceLength = circumerence / CGFloat(dotCount) - dotLength
     }
@@ -45,11 +46,15 @@ struct CircleOfLife: View {
                     .rotationEffect(.init(degrees: -90))
                     .onAppear(perform: {
                         animate()
+                        Task {
+                            await blaha()
+                        }
                     })
                     .onDisappear(perform: {
                         startAnimation = 0
                         endAnimation = 0
                         themeColor = Color.white.opacity(0.3)
+                        timer = 0
                     })
             }
         }
@@ -60,10 +65,33 @@ struct CircleOfLife: View {
             endAnimation = 1
             themeColor = Color.blue
         }
-        withAnimation(.linear(duration: level.inhale.msToSeconds).delay(level.inhale.msToSeconds + delayAnimation)) {
+        withAnimation(.linear(duration: level.exhale.msToSeconds).delay(level.inhale.msToSeconds + delayAnimation)) {
             startAnimation = 1
             themeColor = Color.orange
         }
+        timer = delayAnimation + level.inhale.msToSeconds + level.exhale.msToSeconds
+    }
+    
+    private func blaha() async {
+        try? await Task.sleep(nanoseconds: UInt64(timer * 1000000000))
+        resetCircle()
+        withAnimation(.linear(duration: level.inhale.msToSeconds)) {
+            endAnimation = 1
+            themeColor = Color.blue
+        }
+        withAnimation(.linear(duration: level.inhale.msToSeconds).delay(level.inhale.msToSeconds)) {
+            startAnimation = 1
+            themeColor = Color.orange
+        }
+    }
+    
+    private func resetCircle() {
+        startAnimation = 0
+        endAnimation = 0
+    }
+    
+    private func test() {
+        print("test")
     }
 
 }
